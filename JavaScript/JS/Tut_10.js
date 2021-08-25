8419ad5b107c63c61d41c2dfd3ae2017457f1f22
Kashif-Kamran <i190541@nu.edu.pk>
2021-08-26 02:04:01 +0500

    Start of Output based JS programing on Document of HTML and End of Fundamentals of JS,

---
+++ JavaScript/JS/Tut_10.js
@@ -0,0 +1,18 @@
+// console.log("Dealing With Actual ")
+// let win = window;
+// console.log(win);
+/* ---------------------------------- NOTES --------------------------------- */
+/*  Window Object is global in Java script So Actually it does not mater how we write the the member
+i.e window.alert("Any Alert"); // and
+    alert("AnyAlar");
+        re same
+*/
+
+/* -------------------------- Prompt: taking input -------------------------- */
+//let name = prompt("Enter Your name : ");
+//console.log(`Entered Name : ${name}`);
+/* --------------------------------- Confirm -------------------------------- */
+//let conf = confirm("Are Your sure? ");
+// console.log(`Confirmation: ${conf}`);
+
+/* ---------------- Upper Things Were Old JS Now we have new ---------------- */
---
+++ JavaScript/JS/Tut_11.js
@@ -0,0 +1,4 @@
+let doc = document;
+console.log(doc);
+let docAll = document.all;
+let body = document.body;
